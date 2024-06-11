68af4b0da15c11391b8441eb0be016d43a6061dc
RaviShankar <ravishankar9511@yahoo.com>
2024-06-11 21:46:46 +0530

    Fix eslint error

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-
-
-export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  ...tseslint.configs.recommended,
-];
